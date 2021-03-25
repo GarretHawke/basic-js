321d09816d6caf6d39543486c14968e82fc67895
GarretHawke <alexandra17au@gmail.com>
2021-03-26 00:12:47 +0300

    feat: add solutions on Count cats, Carbon dating, Dream team, Tower of Hanoi

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let sample = 0;
+  if (sampleActivity == null || typeof(sampleActivity) !== 'string') return false;
+  else {
+    sample = parseFloat(sampleActivity);
+  }
+
+  if (isNaN(sample) || sample <= 0) return false;
+  
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let time = Math.ceil(Math.log(MODERN_ACTIVITY / sample) / k);
+  if (time < 0) return false;
+  return time;
 };
